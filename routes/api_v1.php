b48cdc031b1bdfdc58c104f926fe8e27b9cd7198
HomeQu <Quanvle@live.dk>
2022-06-09 15:04:47 +0200

    Working on api versioning

---
+++ routes/api_v1.php
@@ -0,0 +1,5 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::get("index", [App\Http\Controllers\v1\MeetingController::class, "index"]);
---
+++ routes/api_v2.php
@@ -0,0 +1,5 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::get("index", [App\Http\Controllers\v2\MeetingController::class, "index"]);
